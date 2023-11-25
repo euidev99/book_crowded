7769d6e9d0010489df8fe4b510dcacb1e86f2985
chaeyoung <chae00901@naver.com>
2023-11-25 19:32:32 +0900

    mypage logout 기능 추가

--- app/src/main/java/com/example/bookcrowded/ui/my/MyFragment.kt
+++ app/src/main/java/com/example/bookcrowded/ui/my/MyFragment.kt
@@ -1,18 +1,21 @@
 package com.example.bookcrowded.ui.my
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.bookcrowded.R
+import com.example.bookcrowded.common.AuthManager
 import com.example.bookcrowded.databinding.FragmentHomeBinding
 import com.example.bookcrowded.databinding.FragmentMyBinding
 import com.example.bookcrowded.ui.chatlist.ChatListActivity
 import com.example.bookcrowded.ui.common.BaseFragment
+import com.example.bookcrowded.ui.entry.EntryActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MyFragment : BaseFragment() {
     private var _binding: FragmentMyBinding? = null
@@ -34,10 +37,33 @@ class MyFragment : BaseFragment() {
         setView()
     }
 
+//    private fun setView() {
+//        binding.chatLayout.setOnClickListener {
+//            context?.let { it1 -> ChatListActivity.startActivity(it1) }
+//        }
+//    }
+
     private fun setView() {
-        binding.chatLayout.setOnClickListener {
-            context?.let { it1 -> ChatListActivity.startActivity(it1) }
+        binding.logoutButton.setOnClickListener {
+            // Firebase 로그아웃 처리
+            FirebaseAuth.getInstance().signOut()
+
+            // AuthManager의 사용자 정보 초기화
+            AuthManager.userId = ""
+            AuthManager.userEmail = ""
+
+            // 로그아웃 후 EntryActivity로 이동
+            navigateToEntryActivity()
         }
+    }
+    private fun navigateToEntryActivity() {
+        // EntryActivity로 이동하는 인텐트 생성
+        val intent = Intent(requireContext(), EntryActivity::class.java)
+        // 모든 이전 액티비티를 클리어하고 새로 시작
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        // 현재 액티비티 종료
+        activity?.finish()
     }
 
     override fun onDestroyView() {
