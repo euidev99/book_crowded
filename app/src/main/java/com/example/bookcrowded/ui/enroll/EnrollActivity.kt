2f854a5a244b7bc04f28e4c148ce86b142567dee
chaeyoung <chae00901@naver.com>
2023-11-28 21:22:07 +0900

    enroll add

--- app/src/main/java/com/example/bookcrowded/ui/enroll/EnrollActivity.kt
+++ app/src/main/java/com/example/bookcrowded/ui/enroll/EnrollActivity.kt
@@ -2,14 +2,13 @@ package com.example.bookcrowded.ui.enroll
 
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import androidx.core.content.ContextCompat
+import android.widget.Toast
 import com.example.bookcrowded.common.AuthManager
 import com.example.bookcrowded.databinding.ActivityEnrollmentBinding
 import com.example.bookcrowded.ui.common.BaseActivity
 import com.example.bookcrowded.ui.common.BaseRepository
+import com.example.bookcrowded.ui.common.RepoResult
 import com.example.bookcrowded.ui.dto.SellItem
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -26,21 +25,57 @@ class EnrollActivity : BaseActivity() {
         _binding = ActivityEnrollmentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // editSwitch 초기 상태를 true로 설정 (판매 중 상태)
+        binding.editSwitch.isChecked = true
+        binding.editSwitch.text = "판매 중"
+
+        // 스위치 상태 변경 리스너 추가
+        binding.editSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // 판매 중
+                binding.editSwitch.text = "판매 중"
+            } else {
+                // 판매 완료
+                binding.editSwitch.text = "판매 완료"
+            }
+        }
+
         binding.enrollmentButton.setOnClickListener {
+            val isSold = binding.editSwitch.isChecked
+
+            // 사용자 계정 ID 가져오기
+            val userId = AuthManager.userId
+
+            // 스위치 상태에 따라 sold 속성 설정
             CoroutineScope(Dispatchers.IO).launch {
-                itemRepository.addDocument(
+                when (val result = itemRepository.addDocument(
                     SellItem(AuthManager.userId,
-                        "상품명_테스트",
+                        "hihi",
                         "9999",
                         AuthManager.userEmail,
                         "상품 설명",
                         "",
                         false,
                         "2023-11-11",
-                        false))
+                        false
+                    )
+                )) {
+                    is RepoResult.Success -> {
+                        // 등록 성공
+                        runOnUiThread {
+                            showToast("게시물 등록 성공")
+                        }
+                        finish()
+                    }
+                    is RepoResult.Error -> {
+                        // 등록 실패
+                        runOnUiThread {
+                            showToast("게시물 등록 실패")
+                        }
+                    }
+                }
             }
         }
-
         /*binding.addSampleItemSold.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 itemRepository.addDocument(
@@ -74,12 +109,27 @@ class EnrollActivity : BaseActivity() {
 //        }
     }
 
-
+    // showToast 함수 추가
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
     private fun setView() {
         //backButton으로 디테일 화면 종료
         binding.backButton.setOnClickListener {
             finish()
         }
+
+        // 스위치 상태 변경 리스너 추가
+        binding.editSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // 판매 중
+                // 필요한 동작 수행
+            } else {
+                // 판매 완료
+                // 필요한 동작 수행
+            }
+        }
+
 
     }
     override fun onDestroy() {
